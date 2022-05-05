b8d4bb286259e2e0e4f85f0bfaa6607778bd180a
PabloEsk <93121681+PabloEskob@users.noreply.github.com>
2022-05-05 13:29:50 +0300

    First

--- Weapon.cs
+++ Weapon.cs
@@ -1,27 +1,53 @@
 
-    class Weapon
-    {
-        public int Damage;
-        public int Bullets;
+class Weapon
+{
+    private int _damage;
+    private int _bullets;
 
-        public void Fire(Player player)
+    public int Damage => _damage;
+    public int Bullets => _bullets;
+
+    public void TryToShoot(Player player)
+    {
+        if (_bullets > 0)
         {
-            player.Health -= Damage;
-            Bullets -= 1;
+            player.TakeDamage(Damage);
+            _bullets -= 1;
+        }
+    }
+}
+
+class Player
+{
+    private int _health;
+    private bool _isDead;
+
+    public int Health => _health;
+
+    public void TakeDamage(int damage)
+    {
+        if (_isDead) return;
+
+        _health -= damage;
+
+        if (_health <= 0)
+        {
+            Die();
         }
     }
 
-    class Player
+    private void Die()
     {
-        public int Health;
+        _isDead = true;
     }
+}
 
-    class Bot
+class Bot
+{
+    private Weapon _weapon;
+
+    public void OnSeePlayer(Player player)
     {
-        public Weapon Weapon;
-
-        public void OnSeePlayer(Player player)
-        {
-            Weapon.Fire(player);
-        }
+        _weapon.TryToShoot(player);
     }
+}
