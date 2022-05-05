90c6742ba1abcff4421846a26b611db864935d9e
PabloEsk <93121681+PabloEskob@users.noreply.github.com>
2022-05-05 16:47:55 +0300

    Revert "First"

--- Weapon.cs
+++ Weapon.cs
@@ -1,53 +1,27 @@
 
-class Weapon
-{
-    private int _damage;
-    private int _bullets;
-
-    public int Damage => _damage;
-    public int Bullets => _bullets;
-
-    public void TryToShoot(Player player)
+    class Weapon
     {
-        if (_bullets > 0)
+        public int Damage;
+        public int Bullets;
+
+        public void Fire(Player player)
         {
-            player.TakeDamage(Damage);
-            _bullets -= 1;
-        }
-    }
-}
-
-class Player
-{
-    private int _health;
-    private bool _isDead;
-
-    public int Health => _health;
-
-    public void TakeDamage(int damage)
-    {
-        if (_isDead) return;
-
-        _health -= damage;
-
-        if (_health <= 0)
-        {
-            Die();
+            player.Health -= Damage;
+            Bullets -= 1;
         }
     }
 
-    private void Die()
+    class Player
     {
-        _isDead = true;
+        public int Health;
     }
-}
 
-class Bot
-{
-    private Weapon _weapon;
-
-    public void OnSeePlayer(Player player)
+    class Bot
     {
-        _weapon.TryToShoot(player);
+        public Weapon Weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
+        }
     }
-}
